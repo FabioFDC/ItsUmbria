9f3ade73989c662cfb607c6507ff6457ce92cc44
FabioFDC <fabio.del.citto@gmail.com>
2019-11-09 14:34:11 +0100

    Add files via upload

---
+++ Game/Game/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Game.Models;
+
+namespace Game.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public int Test(int a, int b, int c)
+        {
+            int d = a + b + c;
+            return d;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Game/Game/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿function apriChiudiMenu() {
+    $(".menu").toggleClass("chiuso");
+}
+
+function eliminaSplash() {
+    $(".splash").css("display", "none");
+}
+
+
+$("document").ready(function () {
+    window.setTimeout(function () {
+        eliminaSplash();
+    }, 4000);
+});
+
+
+function seleziona(obj) {
+    $(".selected").removeClass("selected");
+    $(obj).addClass("selected");
+}
+
+function redirect() {
+    var myId = $(".selected").attr("id");
+    window.location = "/game/lobby/" + myId;
+}
